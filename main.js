366cc94a58e6289e2e12ae354002512eace2329c
Gisele Granato <gi__@live.com>
2020-06-01 04:35:06 -0300

    mobile version

---
+++ main.js
@@ -0,0 +1,8 @@
+function AbrirMenu(){
+    
+    document.getElementById("menu-list").style.display = "block";
+}
+
+function FecharMenu(){
+    document.getElementById("menu-list").style.display = "none";
+}
