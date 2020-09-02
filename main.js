e77ae3d21f62ccd162ce87dc21cfd7d95c4b0ad3
Gisele Granato <37881099+gfggiza@users.noreply.github.com>
2020-09-02 12:59:00 -0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function AbrirMenu(){
-    
-    document.getElementById("menu-list").style.display = "block";
-}
-
-function FecharMenu(){
-    document.getElementById("menu-list").style.display = "none";
-}
-
